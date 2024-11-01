f8ca458bb9ad6d5af84f0fe226209246f489bd4a
EliasNeerbye <eliasnee@afk.no>
2024-11-01 13:21:29 +0100

    Updated User model

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,5 +1,4 @@
 const mongoose = require('mongoose');
-const bcrypt = require('bcrypt');
 
 const userSchema = new mongoose.Schema({
     username: {
@@ -12,34 +11,10 @@ const userSchema = new mongoose.Schema({
         type: String,
         required: true,
         unique: true,
-        lowercase: true
-    },
-    password: {
-        type: String,
-        required: true
-    },
-    score: {
-        type: Number,
-        default: 0
-    },
-    createdAt: {
-        type: Date,
-        default: Date.now
+        lowercase: true,
+        trim: true
     }
-});
-
-// Hash password before saving user to database
-userSchema.pre('save', async function (next) {
-    if (!this.isModified('password')) return next();
-    const salt = await bcrypt.genSalt(10);
-    this.password = await bcrypt.hash(this.password, salt);
-    next();
-});
-
-// Method to compare entered password with hashed password in DB
-userSchema.methods.comparePassword = async function (enteredPassword) {
-    return await bcrypt.compare(enteredPassword, this.password);
-};
+}, { timestamps: true });
 
 const User = mongoose.model('User', userSchema);
 module.exports = User;

5e76462522cebd24d2ed336fdbff016f549793a6
Lisan-al-gaib <helkion@proton.me>
2024-11-04 10:21:00 +0100

    Added Tailwind and have begun on the landing page

--- frontend/src/pages/CreateUser.jsx
+++ frontend/src/pages/CreateUser.jsx
@@ -5,30 +5,30 @@ export default function CreateUser() {
     const [password, setPassword] = useState();
     const [repeatPassword, setrepeatPassword] = useState();
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        console.log(email)
-        console.log(password)
-        console.log(repeatPassword)
-        axios.post("http://localhost:5000/api/users",
-            {
-                email: email,
-                password: password,
-                repeatPassword: repeatPassword
-            }).then((response) => {
-                console.log(response);
-            }).catch((error) => {
-                console.log("error", error)
-            })
-        )
-    }
+    // const handleSubmit = (e) => {
+    //     e.preventDefault();
+    //     console.log(email)
+    //     console.log(password)
+    //     console.log(repeatPassword)
+    //     axios.post("http://localhost:5000/api/users",
+    //         {
+    //             email: email,
+    //             password: password,
+    //             repeatPassword: repeatPassword
+    //         }).then((response) => {
+    //             console.log(response);
+    //         }).catch((error) => {
+    //             console.log("error", error)
+    //         })
+    //     )
+    // }
 
 
     return (
         <div>
             <h1>Hallo</h1>
 
-            <form onSubmit={handleSubmit}>
+            <form>
                 <input type="email" placeholder="email" onChange={(e) => {setEmail(e.target.value)}}/>
                 <input type="password" placeholder="password" onChange={(e) => {setPassword(e.target.value)}}/>
                 <input type="password" placeholder="repeat password" onChange={(e) => {setrepeatPassword(e.target.value)}}/>

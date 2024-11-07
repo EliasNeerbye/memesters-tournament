1e0c0f2d2431d46eabeea12a0b2c2fe76d9f5159
Lisan-al-gaib <helkion@proton.me>
2024-11-07 10:36:45 +0100

    I have added the login and registration

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import { Routes, Route } from "react-router-dom";
 import LandingPage from "./pages/LandingPage";
 import CreateUser from "./pages/CreateUser";
 import LoginUser from "./pages/LoginUser";
+import ProfilePage from "./pages/ProfilePage";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Route path="/" element={<LandingPage />} />
       <Route path="/createUser" element={<CreateUser />} />
       <Route path="/login" element={<LoginUser />} />
+      <Route path="/profile" element={<ProfilePage />} />
     </Routes>
   );
 }

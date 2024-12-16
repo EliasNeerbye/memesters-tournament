ddb72e7c246c66ac477a74d93590db1028aea684
DeployVM <deployVM@memesters.com>
2024-12-16 10:35:38 +0000

    Rejoin update

--- backend/sockets/handlers/rejoinGameHandler.js
+++ backend/sockets/handlers/rejoinGameHandler.js
@@ -33,34 +33,52 @@ const rejoinGameHandler = (io, socket, activeGames) => async () => {
         const oldSocketId = currentGame.players[playerIndex].socketId;
         currentGame.players[playerIndex].socketId = socket.id;
 
+        // If the game is active in the "activeGames" collection, update its socket connections
         if (activeGames.has(currentGame._id.toString())) {
             const gameData = activeGames.get(currentGame._id.toString());
             gameData.sockets.delete(oldSocketId);
             gameData.sockets.add(socket.id);
         }
 
-        if ((currentGame.state = "playing")) {
+        // Ensure we send the correct state if the game is in the "waiting" or "playing" state
+        if (currentGame.state === "playing") {
+            // Corrected: Use `player.currentMemes` for currentMemes
             return socket.emit("gameRejoined", {
                 gameId: currentGame._id,
                 gameState: currentGame.state,
-                currentMemes: player._id,
+                currentMemes: player.currentMemes,  // Corrected to player.currentMemes
+            });
+        } else if (currentGame.state === "waiting") {
+            socket.emit("gameRejoined", {
+                gameId: currentGame._id,
+                gameState: currentGame.state,
+                host: {
+                    username: currentGame.hostUserId.username,
+                    pfp: currentGame.hostUserId.pfp,
+                },
+                players: currentGame.players.map((p) => ({
+                    username: p.userId.username,
+                    pfp: p.userId.pfp,
+                })),
+            });
+        } else {
+            socket.emit("gameRejoined", {
+                gameId: currentGame._id,
+                gameState: currentGame.state,
+                host: {
+                    username: currentGame.hostUserId.username,
+                    pfp: currentGame.hostUserId.pfp,
+                },
+                players: currentGame.players.map((p) => ({
+                    username: p.userId.username,
+                    pfp: p.userId.pfp,
+                })),
             });
         }
 
-        socket.emit("gameRejoined", {
-            gameId: currentGame._id,
-            gameState: currentGame.state,
-            host: {
-                username: currentGame.hostUserId.username,
-                pfp: currentGame.hostUserId.pfp,
-            },
-            players: currentGame.players.map((p) => ({
-                username: p.userId.username,
-                pfp: p.userId.pfp,
-            })),
-        });
-
         socket.join(currentGame._id.toString());
+
+        // Emit player rejoin to all other connected players
         socket.to(currentGame._id.toString()).emit("playerRejoined", {
             playerInfo: {
                 playerName: player.username,
